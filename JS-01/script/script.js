dc8081123b3daa3183940a7e3c23588cae8772d7
ADevCalledMo <mozeswalker@outlook.com>
2024-01-22 19:13:45 +0000

    JS-01 finished

---
+++ JS-01/script/script.js
@@ -0,0 +1,7 @@
+console.log("23 + 97 =",23 + 97);
+console.log("45 + 12 + 17 + 87 + 23 + 32 =",45 + 12 + 17 + 87 + 23 +32);
+console.log("(4 + 6 + 9) / 77 = ", (4 + 6 + 9) / 77) ;
+
+let a = 10;
+console.log(a, "<-- let a")
+console.log("9 * a =",9 * a);
