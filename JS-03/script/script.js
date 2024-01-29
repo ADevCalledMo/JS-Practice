cc7874b8d9ac80dde784ff258ec8e0da3c254a23
ADevCalledMo <mozeswalker@outlook.com>
2024-01-29 17:08:02 +0000

    finished FizzBuzz

--- JS-03/script/script.js
+++ JS-03/script/script.js
@@ -1,20 +1,31 @@
-let answer = prompt("Please enter a number to play FizzBuzz");
+let delayInMilliseconds = 3000;
 
-while (!/^[0-9]+$/.test(answer)) {
-  alert("Please insert a NUMBER!");
-  answer = prompt("Please enter a number to play FizzBuzz");
-}
-alert("Thanks, you entered a number. Please now check the console.");
-console.log("Number entered:", answer);
+setTimeout(() => {
+  let answer = prompt("Please enter a number to play FizzBuzz");
 
-for (i = 0; i <= answer; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log(i, "<-- FIZZBUZZ");
-  } else if (i > 0 && i % 5 === 0) {
-    console.log(i, "<-- BUZZ");
-  } else if (i > 0 && i % 3 === 0) {
-    console.log(i, "<-- FIZZ");
-  } else {
-    console.log(i);
+  while (!/^[0-9]+$/.test(answer)) {
+    alert("Please insert a NUMBER!");
+    answer = prompt("Please enter a number to play FizzBuzz");
   }
-}
+  alert("Thanks, you entered a number. Please now check the console.");
+  console.log("Number entered:", answer);
+
+  for (i = 1; i <= answer; i++) {
+    if (i % 5 === 0 && i % 3 === 0) {
+      console.log(i, "<-- FIZZBUZZ!!!");
+    } else if (i % 5 === 0) {
+      console.log(i, "<-- BUZZ");
+    } else if (i % 3 === 0) {
+      console.log(i, "<-- FIZZ");
+    } else {
+      console.log(i);
+    }
+  }
+
+  setTimeout(() => {
+    alert("Thank you for playing!! Please reload the page to try again")
+  }, 5000)
+
+}, delayInMilliseconds);
+
+
