b81f96c41b44138245a7f93c45105d03031bf050
ADevCalledMo <mozeswalker@outlook.com>
2024-02-04 04:43:56 +0000

    PlayerSelection from prompt complete

--- JS-04/script/script.js
+++ JS-04/script/script.js
@@ -1,39 +1,45 @@
 let delay = 3000;
-const choices = ["rock", "paper", "scissors"];
+const choices = ["Rock", "Paper", "Scissors"];
 
-const getComputerChoice = ((arr) => {
-    // gets random index
-    const randomIndex = Math.floor(Math.random() * arr.length);
+const getComputerChoice = (arr) => {
+  // gets random index
+  const randomIndex = Math.floor(Math.random() * arr.length);
 
-    //gets random item
-    const item = arr[randomIndex];
+  //gets random item
+  const item = arr[randomIndex];
 
-    return item;
-})
+  return item;
+};
 
-const computerChoice = getComputerChoice(choices).toUpperCase(); 
-
+const computerChoice = getComputerChoice(choices);
 
 alert(
-    "Welcome to Rock Paper Scissors! In a few seconds you will be asked to pick Rock, Paper or Scissors..."
+  "Welcome to Rock Paper Scissors! In a few seconds you will be asked to pick Rock, Paper or Scissors..."
+);
+
+let playerChoice = prompt(
+  "Please enter a number for selection: 1 - ROCK. 2 - PAPER. 3 - SCISSORS."
+);
+
+while (!/^[1-3]+$/.test(playerChoice)) {
+  alert(
+    "Please only insert number from 1 - 3! 1 - ROCK. 2 - PAPER. 3 - SCISSORS"
   );
+  playerChoice = prompt(
+    "Please enter a number for selection: 1 - ROCK. 2 - PAPER. 3 - SCISSORS."
+  );
+}
 
- console.log(getComputerChoice(choices))
- console.log(getComputerChoice(choices))
- console.log(getComputerChoice(choices))
- console.log(getComputerChoice(choices))
- console.log(getComputerChoice(choices))
+if (playerChoice == 1) {
+  playerChoice = "Rock";
+}
+if (playerChoice == 2) {
+  playerChoice = "Paper";
+}
+if (playerChoice == 3) {
+  playerChoice = "Scissors";
+}
 
- console.log(computerChoice)
- console.log(computerChoice)
- console.log(computerChoice)
- console.log(computerChoice)
- console.log(computerChoice)
- 
-
-
-
-// setTimeout(() => {
-
-// }, delay);
-
+console.log(
+  `The Computer Selection is: ${computerChoice} and the Player Selection is: ${playerChoice}`
+);
